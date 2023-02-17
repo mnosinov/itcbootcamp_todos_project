3b5bb5600f6113a5c1b09f334803db2b1b91f93e
mnosinov <mnosinov@gmail.com>
2023-02-17 21:45:40 +0600

    show todos and items left quantity are ready to use

--- js/app-model.js
+++ js/app-model.js
@@ -1,12 +1,42 @@
+/* initial data --------------------------------BEGIN */
 var todos = [
-	{id: 1, title: "Completed online JavaScript course", status: "COMPLETED"},
-	{id: 2, title: "Jog around the park 3x", status: "ACTIVE"},
-	{id: 3, title: "10 minutes meditation", status: "ACTIVE"},
-	{id: 4, title: "Read for 1 hour", status: "ACTIVE"},
-	{id: 5, title: "Pick up groceries", status: "ACTIVE"},
-	{id: 6, title: "Complete Todo App on Frontend Mentor", status: "ACTIVE"}
+	{id: 6, title: "Completed online JavaScript course", status: "COMPLETED"},
+	{id: 5, title: "Jog around the park 3x", status: "ACTIVE"},
+	{id: 4, title: "10 minutes meditation", status: "ACTIVE"},
+	{id: 3, title: "Read for 1 hour", status: "ACTIVE"},
+	{id: 2, title: "Pick up groceries", status: "COMPLETED"},
+	{id: 1, title: "Complete Todo App on Frontend Mentor", status: "ACTIVE"}
 ];
+// add orderNumber, by default it is equal to id
+todos.forEach( item => {
+	item.orderNumber = item.id;
+});
 console.log(todos[1]);
+
+/* initial data --------------------------------END */
+
+// show todos list
+function showTodos() {
+	todosDiv.innerHTML = '';
+
+	// loop in reverse order
+	let orderedTodos = todos.sort( (a, b) => b.orderNumber - a.orderNumber);
+	for (let todo of orderedTodos) {
+		todosDiv.innerHTML += `
+			<div class="todo">
+				<input type="checkbox" class="checkbox" data-id="${todo.id}"
+					${todo.status === 'COMPLETED'? 'checked': ''} >
+				<input type="text" value="${todo.title}" data-id="${todo.id}" class="status-${todo.status}">
+			</div>
+		`;
+	}
+}
+
+// show active items left quantity
+function showItemsleftQnt() {
+	let itemsLeftQnt = todos.filter( item => item.status === 'ACTIVE').length;
+	itemsLeftQntSpan.innerHTML = `${itemsLeftQnt} items left`;
+}
 
 /* theme switcher ---------------------------------- BEGIN */
 themeSwitcherBtn.addEventListener('click', event => {
@@ -26,3 +56,6 @@ themeSwitcherBtn.addEventListener('click', event => {
 	document.body.classList.toggle(themes[nextThemeIndex]);
 });
 /* theme switcher ---------------------------------- END */
+
+showTodos();
+showItemsleftQnt();
--- js/app-view.js
+++ js/app-view.js
@@ -1,2 +1,4 @@
 var themes = ['theme-light', 'theme-dark'];
 var themeSwitcherBtn = document.querySelector('.theme-switcher-btn');
+var todosDiv = document.querySelector('.todos');
+var itemsLeftQntSpan = document.querySelector('.items-left-qnt-span');
